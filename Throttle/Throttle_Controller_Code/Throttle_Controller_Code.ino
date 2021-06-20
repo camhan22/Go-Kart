ef96ff5ccb84e89f9b3fe718e6cfc4b32879fc33
camhan22 <hansoncameron18@yahoo.com>
2021-06-20 00:56:53 -0400

    Added status codes

--- Throttle/Throttle_Controller_Code/Throttle_Controller_Code.ino
+++ Throttle/Throttle_Controller_Code/Throttle_Controller_Code.ino
@@ -2,27 +2,23 @@
   Author: Cameron Hanson
   Author Email: hansoncameron18@yahoo.com
 
-  Revision #: 1.1
-  Revision Release Date: 06/16/2021
+  Revision #: 1.2
+  Revision Release Date: 06/20/2021
 
   Revision History:
+  1.2 - Added fault codes to tell alert the user if a wire has failed on the pots and which one. Also added a reset function from the I2C bus
   1.1 - Added direction header that way the system can function even when mounted backwards.
   1.0 - Initial Release
 
 
   Things to add/update:
-  Add a command to reset the system from a software fault. Might add a pin to allow it to hard reset itself.
-*/
-
-/*
-   NOTES: In order to reset the system, power will need to be switched off and then back on.
-
 */
 //INCLUDES//
 #include <Wire.h>
 
 #define Throttle_1_Pin A3 //Define the pins the throttles are attached to
 #define Throttle_2_Pin A2 //PB3 and PB4
+//Left pot in normal orientation is throttle_1 and right pot is throttle_2
 
 #define Direction_Pin 1 //Pin the direction reversing occurs on
 
@@ -30,13 +26,15 @@
 byte Throttle_1_Value = 0; //Percentage from Throttle 1
 byte Throttle_2_Value = 0; //Percentage for Throttle 2
 //These will be used to check if there is a problem (redundancy since it is electric and not mechanically linked)
-byte Throttle_Percent = 0;
 
 byte ThrottlePercent = 0; // Holds the throttle percentage for the motors
 byte Throttle_Status = 0; // Holds the status of the controller, tells if there was an error or not
 byte Tolerance = 5; //Tolerance in percent. Meaning they cannot differ by more than this amount
 
 byte Controller_Status = 0; //Holds the status of the controller
+int reset_count = 0; //Holds whether the correct number of bytes has been recieved from previous transmissions
+bool reset = 0; //Tells the controller that a reset event has been triggered
+
 
 void setup() {
   //Set the throttle pins as inputs to read the wiper voltage of the potentiometers
@@ -58,14 +56,56 @@ void loop() {
     Throttle_2_Value = map(analogRead(Throttle_2_Pin), 0, 1023, 100, 0); //Map input 2 to 0-100%
   }
 
-  if (abs(Throttle_1_Value - Throttle_2_Value) > Tolerance) {
-    Controller_Status = 1; //Throw a fault code telling that the tolerancing has not been met
+  //FAULTS
+  if (fault == 1) { //If there is a fault, then send that code and make the throttle go to zero to stop the machine
     Throttle_Percent = 0; //Tell the motors to turn off since the throttle data isnt reliable
-  } else {
+    if(reset == 1){ //If a reset event comes in
+      fault = 0; //There are no faults left
+      Controller_Status = 0; //Everything is good to go
+      reset = 0; //The reset has done it's job, now we clear it and wait for the next reset to come in
+    }
+  }
+  //Checks if any of the pots has a broken connection
+  else if(Throttle_2_Value < 100-Tolerance && Throttle_2_Value > 0+Tolerance && Throttle_1_Value == 100){ 
+    //If the first throttle is somewhere between 0 and 100 but the other is pegged at 0, the second throttle has a broken ground connection
+    fault = 1;
+    if(digitalRead(Direction_Pin) == 1){
+      Controller_Status = 2;
+    }else{
+      Controller_Status = 8;
+    }
+  }else if(Throttle_2_Value < 100-Tolerance && Throttle_2_Value > 0+Tolerance && Throttle_1_Value == 0){ 
+    //If the first throttle is somewhere between 0 and 100 but the other is pegged at 0, the second throttle has a broken power connection
+    fault = 1;
+    if(digitalRead(Direction_Pin) == 1){
+      Controller_Status = 3;
+    }else{
+      Controller_Status = 9;
+    }
+  }else if(Throttle_1_Value < 100-Tolerance && Throttle_1_Value > 0+Tolerance && Throttle_2_Value == 100){ 
+    //If the first throttle is somewhere between 0 and 100 but the other is pegged at 100, the second throttle has a broken ground connection
+    fault = 1;
+    if(digitalRead(Direction_Pin) == 1){
+      Controller_Status = 4;
+    }else{
+      Controller_Status = 6;
+    }
+  }else if(Throttle_1_Value < 100-Tolerance && Throttle_1_Value > 0+Tolerance && Throttle_2_Value == 0){ 
+    //If the first throttle is somewhere between 0 and 100 but the other is pegged at 0, the second throttle has a broken power connection
+    fault = 1;
+    if(digitalRead(Direction_Pin) == 1){
+      Controller_Status = 5;
+    }else{
+      Controller_Status = 7;
+    }
+  }
+  else if (abs(Throttle_1_Value - Throttle_2_Value) > Tolerance) { //Check the throttle values are within tolerance of each other
+    fault = 1; //Hold onto the fault until a reset command is sent
+    Controller_Status = 1; //Throw a fault code telling that the tolerancing has not been met
+  }else { //Otherwise, it is safe to send the throttle to the main controller
     Throttle_Percent = (Throttle_1_Value + Throttle_2_Value) / 2;
     Controller_Status = 0;
   }
-  
   delay(200); //Only allow this value to update every 200ms
 }
 
@@ -73,4 +113,21 @@ void loop() {
 void requestEvent() {
   Wire.write(Throttle_Percent); //Write the throttle percentage to the main controller
   Wire.write(Controller_Status); //Write the status of the throttle controller to know if there were any errors
+}
+void receiveEvent(int howMany) {
+  //When any data arrives to the unit, we will check to see if it is a reset command. A reset is the only commands that should be sent to the system but this ensures it is a reset event
+  checkReset(); 
+}
+
+void checkReset() { // Checks to see if the system has been issued the reset command
+  if (recieve_byte == 113) { // If the byte coming in is 112 (Normally not something sent to the controller) then increase the counter
+    reset_count++;
+  }
+  else if (reset_count >= 1 && recieve_byte == 230 $$ fault == 1) { //otherwise reset the counter
+    reset = 1;
+    reset_count = 0;
+  }
+  else { // If the first byte is 112 and the next byte is 229, then the fault will be allowed to clear
+    reset_count = 0;
+  }
 }
